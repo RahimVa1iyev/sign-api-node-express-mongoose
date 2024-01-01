324873dc2e55962bd52d6da28926159c8b26d7fa
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-01 23:38:03 +0400

    create-routes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const jobsRoutes = require('./routes/jobs')
+const authRoutes = require('./routes/auth')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -11,9 +14,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth',authRoutes)
+app.use('/api/v1/jobs',jobsRoutes)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
