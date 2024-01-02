4b15fed61e8ebc63cedaa3a0eb8ccf0454aa95bd
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-02 12:23:14 +0400

    auth-middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,22 @@
+const { UnauthenticatedError } = require('../errors')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const auth = async (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader || !authHeader.startWith('Bearer ')) {
+        throw new UnauthenticatedError('Authentication invalid')
+    }
+
+    const token = authHeader.split(' ')[1]
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SCRET)
+        req.user = { userId: payload.userId, name: payload.name }
+        next()
+
+    } catch (error) {
+        throw new UnauthenticatedError('Authentication invalid')
+    }
+
+}
